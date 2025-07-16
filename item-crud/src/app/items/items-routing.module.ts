e253bea9b9cd6c0d35dc4806509ebbe9100e288f
Paulo <paulo.gusmao@sellbie.com.br>
2025-07-15 21:42:26 -0300

    refactor: ajuste de codigo

--- item-crud/src/app/items/items-routing.module.ts
+++ item-crud/src/app/items/items-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemListComponent } from './item-list/item-list.component';
-import { ItemFormComponent } from './item-form/item-form.component';
 
 const routes: Routes = [
-  { path: '', component: ItemListComponent },
-  { path: 'create', component: ItemFormComponent },
-  { path: 'edit/:id', component: ItemFormComponent }
+  { path: '', component: ItemListComponent }
 ];
 
 @NgModule({
